1536acb7c2b8605bfcc4f011d413e1fd86b84a0f
AakritiBagyal <91174447+AakritiBagyal@users.noreply.github.com>
2021-10-07 22:24:33 +0530

    Add files via upload

---
+++ practical 6/Task 1/Task1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	float c,d;
+	string s1,s2;
+	cout<<"Enter integer values: ";
+	cin>>a;
+	cout<<"Enter string values: ";
+	cin>>s1;
+	cin>>s2;
+	cout<<"Adding string and int: "<<a+s1<<"\n";
+	return 0;
+}
